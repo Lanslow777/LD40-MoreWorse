22590f3c5d906bbd0a911de6e8bc8223353caea6
Lanslow777 <34001882+Lanslow777@users.noreply.github.com>
2017-12-02 20:09:34 +0100

    Shooter Fix

--- Assets/Script/AIScriptShooter.cs
+++ Assets/Script/AIScriptShooter.cs
@@ -28,6 +28,15 @@ public class AIScriptShooter : MonoBehaviour {
 	int fireTime = 0;
 	// Update is called once per frame
 	void Update () {
+		Vector3 dir = target.position - myTransform.position;
+		dir.z = 0.0f; // Only needed if objects don't share 'z' value
+
+		if ((dir.x < 0 && transform.localScale.x > 0) || (dir.x > 0 && transform.localScale.x < 0)) {
+			Vector3 temp = transform.localScale;
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+
 		fireTime++;
 		if (fireTime == rechargeTime) {
 			fireTime = 0;
